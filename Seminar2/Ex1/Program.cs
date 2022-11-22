0e8ea7d677da04cfcd49754cb8fa348e383c785d
Kristina <kkashkareva@mail.ru>
2022-11-22 11:54:09 +0400

    Решение задачи 10

--- Seminar2/Ex1/Program.cs
+++ Seminar2/Ex1/Program.cs
@@ -6,7 +6,27 @@ namespace Ex1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Задача 10: Напишите программу, которая принимает на вход трёхзначное 
+            // число и на выходе показывает вторую цифру этого числа.
+            // 456 -> 5
+            // 782 -> 8
+            // 918 -> 1
+
+            int GetRandomNumber(int start, int end)
+            {
+                int result = 0;
+                result = new Random().Next(start, end + 1);
+                return result;
+            }
+            int GetFirstLastNumber (int RandomNumber)
+            {
+                int number2 = (RandomNumber / 10) % 10;
+                return number2;
+            }
+            int RandomNumber = GetRandomNumber(100, 999);
+            int number2 = GetFirstLastNumber(RandomNumber);
+            Console.WriteLine($"в числе {RandomNumber} вторая цифра {number2}");
+            
         }
     }
 }
