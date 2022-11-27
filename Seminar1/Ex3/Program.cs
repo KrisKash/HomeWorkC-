68ce207173ed377d1d9b1fd2b54fd29bd5bd8db5
Kristina <kkashkareva@mail.ru>
2022-11-27 13:14:08 +0400

    Решение задачи 3 семинар 1

---
+++ Seminar1/Ex3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Ex3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Number = new int();
+            Console.WriteLine("Введите число:");
+            Number = Convert.ToInt32(Console.ReadLine());
+            if(Number % 2 == 0)
+                Console.WriteLine("Четное");
+            else
+                Console.WriteLine("Нечетное");
+        }
+    }
+}
