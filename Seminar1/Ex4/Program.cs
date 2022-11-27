60d1e646cd272219b1f4ad630785dff779e8b561
Kristina <kkashkareva@mail.ru>
2022-11-27 13:16:18 +0400

    Решение задачи 4 семинар 1

---
+++ Seminar1/Ex4/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Ex4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = new int();
+            Console.WriteLine("Введите число:");
+            N = Convert.ToInt32(Console.ReadLine());
+            int i = 0;
+            Console.WriteLine("Все четные числа:");
+            while (i < N - 1)
+            {
+                i +=2;
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
