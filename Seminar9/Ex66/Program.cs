93f3f93340ee95d7d07116a0588422f15a3a8d12
Kristina <kkashkareva@mail.ru>
2022-12-04 14:48:45 +0400

    Задача 66

---
+++ Seminar9/Ex66/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Ex66
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Задача 66: Задайте значения M и N. Напишите программу, которая 
+            найдёт сумму натуральных элементов в промежутке от M до N.
+            M = 1; N = 15 -> 120
+            M = 4; N = 8. -> 30
+            */
+            
+
+            int GetUserInt(string message)
+            {
+                Console.ForegroundColor = ConsoleColor.DarkCyan;
+                Console.Write (message);
+                Console.ResetColor();
+                int userInt = Convert.ToInt32(Console.ReadLine());
+                return userInt;
+            }
+            void GetSumOfDigit (int userNumberM, int userNumberN, int sum)
+            {
+                if (userNumberM > userNumberN)
+                {
+                    Console.WriteLine($"Сумма чисел от М до N = {sum}"); 
+                    return;             
+                }
+                sum = sum + (userNumberM++);                
+                GetSumOfDigit(userNumberM, userNumberN, sum);
+            }
+            int userNumberM = GetUserInt ("Введите число М: ");
+            int userNumberN = GetUserInt ("Введите число N: ");
+            GetSumOfDigit (userNumberM, userNumberN, 0);
+        }
+    }
+}
